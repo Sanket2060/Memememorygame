f023b34314c08d67499b175dff819f256092c129
Sanket <sanketkarki2060@gmail.com>
2023-12-22 18:41:31 +0545

    countadded

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,12 @@ import {BhupendraJogi,
 
 function App() {
   const forRefs = Array.from({ length: 12 }, () => useRef(null));
-  const [lastMeme,setLastMeme]=useState();
-  const [presentMeme,setPresentMeme]=useState(105);
+  const [lastMeme,setLastMeme]=useState({image:'haha.jpeg',myIndex:1245});
+  const [presentMeme,setPresentMeme]=useState({image:'sad.jpeg',myIndex:7894});
   const [shuffle1,setShuffle1]=useState([]);
   const [shuffle2,setShuffle2]=useState([]);
+  const [highScore,setHighScore]=useState('Not set');
+  const [count,setCount]=useState(-1);
 
   const shuffle = (array) => { 
     return array.map((a) => ({ sort: Math.random(), value: a }))
@@ -33,9 +35,9 @@ function App() {
       setLastMeme(Lastmeme)
 
   }
-  const setPresentMemeFunc=(Presentmemeid)=>{
+  const setPresentMemeFunc=(meme)=>{
     setLastMeme(presentMeme);
-    setPresentMeme(Presentmemeid);
+    setPresentMeme(meme);
   }
 
   useEffect(()=>{
@@ -84,13 +86,30 @@ function App() {
   },[])
 
   useEffect(()=>{
-    if (presentMeme==lastMeme){
+    if (presentMeme.image==lastMeme.image && presentMeme.myIndex!=lastMeme.myIndex){
         console.log("Inside equal condition");
-        forRefs[presentMeme].current.classList.remove('invisible');
-        forRefs[presentMeme].current.classList.add('pointer-events-none');
+        forRefs[presentMeme.myIndex].current.classList.remove('invisible');
+        forRefs[presentMeme.myIndex].current.classList.add('pointer-events-none');
+        forRefs[lastMeme.myIndex].current.classList.remove('invisible');
+        forRefs[lastMeme.myIndex].current.classList.add('pointer-events-none');
     }
+
   },[presentMeme,lastMeme])
+  useEffect(()=>{
+      setCount((count)=>count+1);
+  },[presentMeme])
  
+  // useEffect(()=>{
+  //   if ((presentMeme.myIndex>5 && (presentMeme-6)==lastMeme.myIndex) || (lastMeme.myIndex>5 && (lastMeme-6)==presentMeme.myIndex)){
+  //       console.log("Inside equal condition");
+  //       forRefs[presentMeme.myIndex].current.classList.remove('invisible');
+  //       forRefs[presentMeme.myIndex].current.classList.add('pointer-events-none');
+  //       forRefs[lastMeme.myIndex].current.classList.remove('invisible');
+  //       forRefs[lastMeme.myIndex].current.classList.add('pointer-events-none');
+  //   }
+  // },[presentMeme,lastMeme])
+
+
   useEffect(()=>{
     console.log('Last meme and present meme:',lastMeme,presentMeme)
   },[presentMeme,lastMeme])
@@ -105,9 +124,10 @@ function App() {
           <div className="heading text-5xl">Memes</div>
           <a   href="https://github.com/Sanket2060/Memememorygame" target='_blank' ><div className="githubicon pt-3"><FaGithub  size={32} /></div> </a>
         </header>
-        <div className="game flex w-[95vw] h-[110vw]  border border-blue-700 flex-wrap  justify-center items-center">
+        <div className="game flex w-[95vw] h-[110vw]  border border-blue-700 flex-wrap  justify-center items-center mb-5">
           {
            shuffle1?shuffle1.map((obj,index)=>{
+
                 //  console.log("id:",obj.id);
               // console.log("obj",obj);
               // console.log('memeName',obj.memeName);
@@ -117,8 +137,9 @@ function App() {
             id={`${obj.id}`} 
             SetLastMemeFunc={SetLastMemeFunc} 
             SetPresentMemeFunc={setPresentMemeFunc}
-            ref={forRefs[obj.id]} 
+            ref={forRefs[index]} 
             key={index}
+            myIndex={index}
             />
             }):null
           }
@@ -127,6 +148,7 @@ function App() {
 
           {
             shuffle2?shuffle2.map((obj,index)=>{
+               const newindex=index+6;
               // console.log("id:",obj.id);
               // console.log("obj",obj);
               // console.log('memeName',obj.memeName);
@@ -136,13 +158,18 @@ function App() {
               id={`${obj.id}`} 
               SetLastMemeFunc={SetLastMemeFunc} 
               SetPresentMemeFunc={setPresentMemeFunc} 
-              ref={forRefs[obj.id]} 
-              key={index}
+              ref={forRefs[newindex]} 
+               key={index}
+              myIndex={newindex}
               />
             }):null
 
           }
 
+        </div>
+        <div className="scorecard bg-black flex justify-around text-2xl px-4 w-full py-4">
+          {/* <div className="highscore">HighScore:{highScore}</div> */}
+          <div className="count">Count:{count}</div>
         </div>
       </div>
     </>
--- src/components/SingleMeme.jsx
+++ src/components/SingleMeme.jsx
@@ -1,15 +1,16 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
-function Singlememe({image,audio,setLastMemeFunc,SetPresentMemeFunc,id},forRefs) {
-  const memeRef=useRef();
+function Singlememe({image,audio,setLastMemeFunc,SetPresentMemeFunc,id,key,myIndex},forRefs) {
+  // const memeRef=useRef();
   const audioRef=useRef();
   const showImage=()=>{
     audioRef.current.playbackRate=1.75;
-    memeRef.current.classList.remove('invisible');
+    forRefs.current.classList.remove('invisible');
     audioRef.current.play();
     setTimeout(() => {
-      memeRef.current.classList.add('invisible');
+      forRefs.current.classList.add('invisible');
       audioRef.current.pause();
+      memeClicked({image,myIndex});
     }, 500);
   }
 
@@ -17,10 +18,15 @@ function Singlememe({image,audio,setLastMemeFunc,SetPresentMemeFunc,id},forRefs)
       SetPresentMemeFunc(value);
 
     } 
+    // useEffect(()=>{
+    //   console.log("Key:",key);
+    // })
   return (
-   <div className="square bg-black w-[22vw] border border-black  h-[22vw] m-3 hover:cursor-pointer" onClick={()=>{
+   <div className="square bg-black w-[22vw] border border-black  h-[22vw] m-3 hover:cursor-pointer" 
+   onClick={()=>{
     showImage();
-    memeClicked(id);
+    console.log("myIndex",myIndex);
+    // memeClicked({image,myIndex});
    }
   }
   //  style={{
@@ -32,9 +38,9 @@ function Singlememe({image,audio,setLastMemeFunc,SetPresentMemeFunc,id},forRefs)
   // }}
   >
     <audio src={`${audio}`} ref={audioRef} ></audio>
-    <div className="imagewrap w-fit h-fit" ref={forRefs}>
-    <img src={`${image}`} className='h-full w-auto max-w-[100%] invisible' ref={memeRef}   />
-    </div>
+    {/* <div className="imagewrap w-fit h-fit" ref={forRefs}> */}
+    <img src={`${image}`} className='h-full w-auto max-w-[100%] invisible' ref={forRefs}   />
+    {/* </div> */}
   </div>
   )
 }

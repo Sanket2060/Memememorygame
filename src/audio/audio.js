5d4b822f4babecca4f7a49551fd1b8f584214814
Sanket <sanketkarki2060@gmail.com>
2023-12-23 12:56:38 +0545

    deploymenterrorpush

--- src/audio/audio.js
+++ src/audio/audio.js
@@ -2,7 +2,7 @@ import  Bhupendrajogi  from "./Bhupendrajogi.mp3"
 import Jaatpaatmai from './Jaatpaatmai.mp3'
 import Lookinglikeawow from './Lookinglikeawow.mp3'
 import Maharibahadur from './Maharibahadur.mp3'
-import Puneetsuperstar from './PUneetsuperstar.mp3'
+import Puneetsuperstar from './Puneetsuperstar.mp3'
 import Thalaforareason from './Thalaforareason.mp3'
 
 export {
